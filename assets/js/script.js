b06077b91a08a159afa8bcdc36512391f56efd29
Vankoekelberg <Mathis>
2021-08-02 20:01:14 +0200

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,21 +96,24 @@ function scrollUpRotate() {
 let showButton = 0;
 document.getElementById("burgerMenu").addEventListener("click", () => {
     showButton++;
-    document.getElementById("showButton1").removeAttribute("style");
-    document.getElementById("showButton2").removeAttribute("style");
-    document.getElementById("showButton3").removeAttribute("style");
-    document.getElementById("showButton4").removeAttribute("style");
-    document.getElementById("showButton5").removeAttribute("style");
-    document.getElementById("showButton6").removeAttribute("style");
-    document.getElementById("showButton7").removeAttribute("style");
+    document.getElementById("topNavBurger").setAttribute("style", "height: 430px");
+    document.getElementById("burgerMenu").setAttribute("style", "transform: rotate(90deg)");
     if(showButton === 2 ) {
-        document.getElementById("showButton1").setAttribute("style", "display: none;");
-        document.getElementById("showButton2").setAttribute("style", "display: none;");
-        document.getElementById("showButton3").setAttribute("style", "display: none;");
-        document.getElementById("showButton4").setAttribute("style", "display: none;");
-        document.getElementById("showButton5").setAttribute("style", "display: none;");
-        document.getElementById("showButton6").setAttribute("style", "display: none;");
-        document.getElementById("showButton7").setAttribute("style", "display: none;");
+        document.getElementById("burgerMenu").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurger").setAttribute("style", "height: 55px");
         showButton = 0;
+    }
+})
+
+
+let showButton1 = 0;
+document.getElementById("burgerMenuDesk").addEventListener("click", () => {
+    showButton1++;
+    document.getElementById("topNavBurgerDesk").setAttribute("style", "width: 90%; height: 14px;");
+    document.getElementById("burgerMenuDesk").setAttribute("style", "transform: rotate(90deg)");
+    if(showButton1 === 2 ) {
+        document.getElementById("burgerMenuDesk").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurgerDesk").setAttribute("style", "width: 20px; height: 9px;");
+        showButton1 = 0;
     }
 })
