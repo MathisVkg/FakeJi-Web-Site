e8dc44975c9f20e5fd4db502c72247394453b343
MathisVkg <mvk181@gmail.com>
2021-08-10 10:11:03 +0200

    edit

--- assets/js/store.js
+++ assets/js/store.js
@@ -142,3 +142,38 @@ function pauseVid1() {
 function pauseVid2() {
   showreelvid.pause();
 }
+
+
+let showButton1 = 0;
+document.getElementById("burgerMenuDesk").addEventListener("click", () => {
+    showButton1++;
+    document.getElementById("topNavBurgerDesk").setAttribute("style", "width: 90%; height: 14px;");
+    document.getElementById("burgerMenuDesk").setAttribute("style", "transform: rotate(90deg)");
+    if(showButton1 === 2 ) {
+        document.getElementById("burgerMenuDesk").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurgerDesk").setAttribute("style", "width: 20px; height: 9px;");
+        showButton1 = 0;
+    }
+    setTimeout(() => {
+        document.getElementById("burgerMenuDesk").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurgerDesk").setAttribute("style", "width: 20px; height: 9px;");
+        showButton1 = 0;
+    }, 5000);
+});
+
+let showButton = 0;
+document.getElementById("burgerMenu").addEventListener("click", () => {
+    showButton++;
+    document.getElementById("topNavBurger").setAttribute("style", "height: 430px");
+    document.getElementById("burgerMenu").setAttribute("style", "transform: rotate(90deg)");
+    if(showButton === 2 ) {
+        document.getElementById("burgerMenu").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurger").setAttribute("style", "height: 55px");
+        showButton = 0;
+    }
+    setTimeout(() => {
+        document.getElementById("burgerMenu").setAttribute("style", "transform: rotate(0deg)");
+        document.getElementById("topNavBurger").setAttribute("style", "height: 55px");
+        showButton = 0;
+    }, 5000);
+});
